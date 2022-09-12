7560f0b6b612602bcf7bedd6a8fe29a866dc4b35
Francisco Ribeiro <francisco.ribeiro@ua.pt>
2022-09-12 17:24:30 +0100

    Deploy globally if no guild id specified

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const token = process.env["TOKEN"];
 const clientId = process.env["CLIENT_ID"];
-const guildId = "";
+const guildId = process.env["GUILD_ID"];
 
 const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Check the bot ping'),
@@ -15,6 +15,12 @@ const commands = [
 
 const rest = new REST({ version: '10' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then((data: any) => console.log(`Successfully registered ${data.length} application commands.`))
-	.catch(console.error);
+if (guildId) {
+	rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+		.then((data: any) => console.log(`Successfully registered ${data.length} application commands on guild ${guildId}.`))
+		.catch(console.error);
+} else {
+	rest.put(Routes.applicationCommands(clientId), { body: commands })
+		.then((data: any) => console.log(`Successfully registered ${data.length} global application commands.`))
+		.catch(console.error);
+}
