7dbc1f06bbc279ed9a66aa8eb24015e4f4e96604
Francisco Ribeiro <francisco.ribeiro@ua.pt>
2022-09-13 17:27:58 +0100

    Log all the registered commands on startup

--- src/CommandRegistry.ts
+++ src/CommandRegistry.ts
@@ -8,15 +8,18 @@ class CommandRegistry {
     public registerCommands() {
         // Read all files in the commands folder
         // Each file has a run function that takes in the interaction
+        const registeredCommands: string[] = [];
         readdirSync(__dirname + "/commands").forEach((file) => {
             const commandFile = require(__dirname + `/commands/${file}`);
             if (!commandFile.default) return;
             const command = new commandFile.default();
             if (command instanceof Command) {
                 const commandName = file.split(".")[0]!;
+                registeredCommands.push(commandName);
                 this.setCommand(commandName, command);
             }
         });
+        console.log(`Registered ${registeredCommands.length} commands: ${registeredCommands.join(", ")}`);
     }
 
     private setCommand(commandName: string, command: Command): void {
