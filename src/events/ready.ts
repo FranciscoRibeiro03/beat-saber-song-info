04bee562c952776e7350e185c36fef8725360b1b
Francisco Ribeiro <francisco.ribeiro@ua.pt>
2022-09-13 17:27:20 +0100

    Add activity to bot

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,6 @@
-import type { Client } from "discord.js";
+import { ActivityType, Client } from "discord.js";
 
 export function run(client: Client) {
+    client.user?.setPresence({ activities: [{ name: "Beat Saber", type: ActivityType.Playing }] })
     console.log(`Ready! Logged in as ${client.user?.tag ?? "unknown"}`);
 }
