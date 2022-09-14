c26830d66b9f40c92b15e3a6f1b53c1c8aa872c8
Francisco Ribeiro <francisco.ribeiro@ua.pt>
2022-09-14 13:25:30 +0100

    Change info command

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -15,12 +15,13 @@ export default class CreatorCommand extends Command {
         if (this.creatorInfo.id) contactInfo += ` Discord: <@${this.creatorInfo.id}>.`;
         if (this.creatorInfo.website) contactInfo += `\nYou can visit their website at ${this.creatorInfo.website}.`;
         if (this.creatorInfo.patreon || this.creatorInfo.paypal || this.creatorInfo.kofi) {
-            contactInfo += "\n\nYou can support them by donating to them at:";
-            if (this.creatorInfo.patreon) contactInfo += `\n- Patreon: ${this.creatorInfo.patreon}`;
-            if (this.creatorInfo.kofi) contactInfo += `\n- Ko-fi: ${this.creatorInfo.kofi}`;
-            if (this.creatorInfo.paypal) contactInfo += `\n- PayPal: ${this.creatorInfo.paypal}`;
+            contactInfo += "\n\nYou can support them by donating to them on:";
+            if (this.creatorInfo.patreon) contactInfo += `\n- [Patreon](${this.creatorInfo.patreon})`;
+            if (this.creatorInfo.kofi) contactInfo += `\n- [Ko-fi](${this.creatorInfo.kofi})`;
+            if (this.creatorInfo.paypal) contactInfo += `\n- [PayPal](${this.creatorInfo.paypal})`;
         }
-        if (this.creatorInfo.supportServer) contactInfo += `\n\nSupport server: ${this.creatorInfo.supportServer}.`;
+        contactInfo += `\n\n[GitHub Repository](https://github.com/FranciscoRibeiro03/beat-saber-song-info)`;
+        if (this.creatorInfo.supportServer) contactInfo += `\n[Go to Support Server](${this.creatorInfo.supportServer})`;
 
         const embed = new BSSIEmbed(interaction.client.user!, interaction.user)
             .setTitle("Info")
