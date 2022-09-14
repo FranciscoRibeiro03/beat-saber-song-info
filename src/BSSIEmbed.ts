90b160493c73a7c2a54c05c8d61d8b28b5774bb4
Francisco Ribeiro <francisco.ribeiro@ua.pt>
2022-09-14 21:03:57 +0100

    Change embed generation to BSSIEmbed class Allows for code reutilisation later

--- src/BSSIEmbed.ts
+++ src/BSSIEmbed.ts
@@ -1,3 +1,4 @@
+import type { MapDetail } from "beatsaver-api/lib/models/MapDetail";
 import { EmbedBuilder, User } from "discord.js";
 
 export default class BSSIEmbed extends EmbedBuilder {
@@ -7,5 +8,33 @@ export default class BSSIEmbed extends EmbedBuilder {
         this.setAuthor({ name: botUser.tag, iconURL: botUser.displayAvatarURL() });
         this.setTimestamp();
         if (requestedBy) this.setFooter({ text: `Requested by: ${requestedBy.tag}`, iconURL: requestedBy.displayAvatarURL() });
+    }
+
+    public static getMapEmbed(botUser: User, requestedBy: User, map: MapDetail): BSSIEmbed {
+        const mods: string[] = [];
+        for (const diff of map.versions[0]!.diffs) {
+            if (diff.chroma && !mods.includes("Chroma")) mods.push("Chroma");
+            if (diff.ne && !mods.includes("Noodle Extensions")) mods.push("Noodle Extensions");
+            if (diff.me && !mods.includes("Mapping Extensions")) mods.push("Mapping Extensions");
+            if (diff.cinema && !mods.includes("Cinema")) mods.push("Cinema");
+        }
+
+        const embed = new BSSIEmbed(botUser, requestedBy)
+            .setTitle(map.name)
+            .setDescription(map.description)
+            .addFields(
+                { name: "Key", value: map.id, inline: true },
+                { name: "Uploader", value: `[${map.uploader.name}](https://beatsaver.com/profile/${map.uploader.id.toString()})`, inline: true },
+                { name: "Uploaded At", value: new Date(map.uploaded).toUTCString(), inline: true },
+                { name: "Upvotes", value: map.stats.upvotes.toString(), inline: true },
+                { name: "Downvotes", value: map.stats.downvotes.toString(), inline: true },
+                { name: "Rating", value: (map.stats.score * 100).toString() + "%", inline: true },
+                { name: "Links", value: `[ZIP Download](${map.versions[0]!.downloadURL}) / [Preview](https://skystudioapps.com/bs-viewer/?id=${map.id})`, inline: true },
+                { name: "Suggested/Required Mods", value: mods.length > 0 ? mods.join(", ") : "None", inline: true },
+            )
+            .setThumbnail(map.versions[0]!.coverURL)
+            .setURL(`https://beatsaver.com/maps/${map.id}`);
+
+        return embed;
     }
 }
--- src/commands/bsr.ts
+++ src/commands/bsr.ts
@@ -38,29 +38,7 @@ export default class BSRCommand extends Command {
             return;
         }
 
-        const mods: string[] = [];
-        for (const diff of map.versions[0]!.diffs) {
-            if (diff.chroma && !mods.includes("Chroma")) mods.push("Chroma");
-            if (diff.ne && !mods.includes("Noodle Extensions")) mods.push("Noodle Extensions");
-            if (diff.me && !mods.includes("Mapping Extensions")) mods.push("Mapping Extensions");
-            if (diff.cinema && !mods.includes("Cinema")) mods.push("Cinema");
-        }
-
-        const embed = new BSSIEmbed(interaction.client.user!, interaction.user)
-            .setTitle(map.name)
-            .setDescription(map.description)
-            .addFields(
-                { name: "Key", value: map.id, inline: true },
-                { name: "Uploader", value: `[${map.uploader.name}](https://beatsaver.com/profile/${map.uploader.id.toString()})`, inline: true },
-                { name: "Uploaded At", value: new Date(map.uploaded).toUTCString(), inline: true },
-                { name: "Upvotes", value: map.stats.upvotes.toString(), inline: true },
-                { name: "Downvotes", value: map.stats.downvotes.toString(), inline: true },
-                { name: "Rating", value: (map.stats.score * 100).toString() + "%", inline: true },
-                { name: "Links", value: `[ZIP Download](${map.versions[0]!.downloadURL}) / [Preview](https://skystudioapps.com/bs-viewer/?id=${map.id})`, inline: true },
-                { name: "Suggested/Required Mods", value: mods.length > 0 ? mods.join(", ") : "None", inline: true },
-            )
-            .setThumbnail(map.versions[0]!.coverURL)
-            .setURL(`https://beatsaver.com/maps/${map.id}`);
+        const embed = BSSIEmbed.getMapEmbed(interaction.client.user!, interaction.user, map);
 
         await interaction.reply({
             embeds: [embed],
