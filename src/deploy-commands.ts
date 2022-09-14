aa26b58860be60596f882a574c46c667961dfc99
Francisco Ribeiro <francisco.ribeiro@ua.pt>
2022-09-14 23:44:57 +0100

    Format commands on deploy, better readability

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -8,14 +8,21 @@ const guildId = process.env["GUILD_ID"];
 
 const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Check the bot ping'),
+
 	new SlashCommandBuilder().setName('invite').setDescription('Get the invite link for the bot'),
+
 	new SlashCommandBuilder().setName('info').setDescription('Get info about the bot'),
+
 	new SlashCommandBuilder().setName('help').setDescription('Get help on how to use the bot'),
+
 	new SlashCommandBuilder().setName('copyright').setDescription('Get the copyright notice for this bot'),
+
 	new SlashCommandBuilder().setName('bsr').setDescription('Get info about a song on BeatSaver using the song key')
 		.addStringOption(option => option.setName('key').setDescription('The song key').setRequired(true)),
+
 	new SlashCommandBuilder().setName('hash').setDescription('Get info about a song on BeatSaver using the song hash')
 		.addStringOption(option => option.setName('hash').setDescription('The song hash').setRequired(true)),
+
 ].map(command => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(token!);
