6a6ac2cf0738808226610f0ad125587d41e74477
Francisco Ribeiro <francisco.ribeiro@ua.pt>
2022-09-15 01:06:40 +0100

    Add bot version on all embeds

--- src/BSSIEmbed.ts
+++ src/BSSIEmbed.ts
@@ -5,7 +5,7 @@ export default class BSSIEmbed extends EmbedBuilder {
     constructor(botUser: User, requestedBy?: User) {
         super();
         this.setColor("#00CC00");
-        this.setAuthor({ name: botUser.tag, iconURL: botUser.displayAvatarURL() });
+        this.setAuthor({ name: `${botUser.tag} | v${process.env["npm_package_version"] ?? "Unknown"}`, iconURL: botUser.displayAvatarURL() });
         this.setTimestamp();
         if (requestedBy) this.setFooter({ text: `Requested by: ${requestedBy.tag}`, iconURL: requestedBy.displayAvatarURL() });
     }
